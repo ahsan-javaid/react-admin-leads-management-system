98570a5dea0b7afe92111bd5156bceef2507327e
Ahsan Javaid <ahsan786cs@gmail.com>
2018-07-14 17:13:06 +0500

    first commit

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+
+class Login extends Component {
+
+  handleClick = () => {
+    console.log('HERE!');
+  };
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="5">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <h1>Login</h1>
+                    <p className="text-muted">Sign In to your account</p>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Username" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" placeholder="Password" />
+                    </InputGroup>
+                    <Row>
+                      <Col xs="6">
+                      <Button onClick={this.handleClick} color="primary" className="px-4">Login</Button>
+                      </Col>
+                      <Col xs="6" className="text-right">
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;
